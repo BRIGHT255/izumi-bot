ea7bde696715d938e3a396c02d3ba2376eb9c426
Chinozi <chinozibright262@gmail.com>
2025-05-25 20:58:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,40 +14,40 @@ global.eypzApi = 'https://api2.eypz.infy.uk/'
 global.Api = 'https://api.eypz.infy.uk/'
 
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
-DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+DEBUG = process.env.DEBUG === undefined ? True : convertToBool(process.env.DEBUG);
 
 module.exports = {
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   BRANCH: "main",
-  ADMIN_ACCESS: toBool(process.env.ADMIN_ACCESS) || false,
+  ADMIN_ACCESS: toBool(process.env.ADMIN_ACCESS) || true ,
   MODE: (process.env.MODE || 'private').toLowerCase(),
   ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
-  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE) || false,
+  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE) || true,
   WARN_COUNT: process.env.WARN_COUNT  || '3',
-  AUTO_STATUS_VIEW: toBool(process.env.AUTO_STATUS_VIEW) || false,
-  SESSION_ID: process.env.SESSION_ID || "izumi session",
+  AUTO_STATUS_VIEW: toBool(process.env.AUTO_STATUS_VIEW) || true ,
+  SESSION_ID: process.env.SESSION_ID ||" izumi~kututenoye,"
   MENU_URL: process.env.MENU_URL || "https://cdn.eypz.ct.ws/url/15-05-25_06-18_w9fi.png",
   CAPTION: process.env.CAPTION || "Made with 🤍",
   READ_MSG: toBool(process.env.READ_MSG),
-  OWNER_NAME: process.env.OWNER_NAME || "Eʏᴘᴢ",
+  OWNER_NAME: process.env.OWNER_NAME || "chinozbright",
   BOT_NAME: process.env.BOT_NAME || "Iᴢᴜᴍɪ-ᴍᴅ",
-  SUDO: process.env.SUDO || null,
+  SUDO: process.env.SUDO || 0789598617 ,
   LANG: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE.toUpperCase(),
   STICKER_PACKNAME: process.env.STICKER_PACKNAME || "Iᴢᴜᴍɪ-ᴍᴅ,Eʏᴘᴢ",
   AUDIO_DATA: process.env.AUDIO_DATA || "Eʏᴘᴢ;Iᴢᴜᴍɪ-ᴍᴅ;https://cdn.eypz.ct.ws/url/15-05-25_06-18_w9fi.png",
   PROCESSNAME: process.env.PROCESSNAME || "Iᴢᴜᴍɪ-ᴍᴅ",
   AUTHOR: process.env.AUTHOR || "Eʏᴘᴢ",
   PRESENCE: process.env.PRESENCE || "unavailable", //available, typing, recording
-  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || True ,
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   KOYEB_API_KEY: process.env.KOYEB_API_KEY || "api",
-  KOYEB_APP_NAME: process.env.KOYEB_APP_NAME || "name",
-  KOYEB: toBool(process.env.KOYEB) || false,
-  HEROKU: toBool(process.env.HEROKU) || false,
-  TERMUX: toBool(process.env.TERMUX) || false,
+  KOYEB_APP_NAME: process.env.KOYEB_APP_NAME || "bright",
+  KOYEB: toBool(process.env.KOYEB) || true ,
+  HEROKU: toBool(process.env.HEROKU) || true ,
+  TERMUX: toBool(process.env.TERMUX) || true ,
   DATABASE_URL: DATABASE_URL,
   DATABASE:
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
